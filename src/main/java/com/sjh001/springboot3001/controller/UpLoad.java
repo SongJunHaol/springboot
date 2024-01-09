4e74c2cea1d3e5658e06dc2662746d4328d76059
sjh <1870892250@qq.com>
2024-01-09 21:08:08 +0800

    测试1.9

--- src/main/java/com/sjh001/springboot3001/controller/UpLoad.java
+++ src/main/java/com/sjh001/springboot3001/controller/UpLoad.java
@@ -4,20 +4,35 @@ import com.sjh001.springboot3001.bean.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.IOException;
 
 @Controller
 @Slf4j
 public class UpLoad {
     @PostMapping("/upload")
+    @ResponseBody
     public Result<String> upLoad(MultipartFile file){
+
+        String path = this.getClass().getResource("").getPath();
+        System.err.println("path：" + path);
+
+        String path3 = System.getProperty("user.dir");
+        System.err.println("path3：" + path3);
+
+
+
+
+
+
+
         try {
             log.info(String.valueOf(file.getBytes()));
-//            System.out.println(file.getBytes());
+            System.out.println(file.getBytes());
+            byte[] c = file.getBytes();
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            System.out.println("上传失败");
         }
         return Result.success("图片上传成功");
     }
